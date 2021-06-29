a8c50e1f341b0536d6d5a29c02c061019251f725
TinkGu <tlwrite@126.com>
2021-06-29 15:27:55 +0800

    feat: setup

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  env: {
+    jest: true,
+    browser: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    camelcase: 'off',
+    'no-use-before-define': 'off',
+    'no-underscore-dangle': 'off',
+    'no-bitwise': 'off',
+    'no-restricted-syntax': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'no-undef': 'off',
+        'no-unused-vars': 'off',
+        'no-restricted-globals': 'off',
+        'no-use-before-define': 'off',
+        'no-plusplus': 'off',
+        'no-redeclare': 'off',
+      },
+    },
+  ],
+};
---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
+        loose: true,
+        targets: {
+          browsers: 'defaults',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: ['@babel/plugin-proposal-object-rest-spread'],
+};
